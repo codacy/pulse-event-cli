7abc2180ac9331d4b2d9e9509899f797f333ddd8
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2020-12-18 15:57:44 +0000

    doc: Edit and improve the help strings

--- cmd/push/push.go
+++ cmd/push/push.go
@@ -17,9 +17,9 @@ var PushCmd = &cobra.Command{
 
 func init() {
 	cmd.RootCmd.AddCommand(PushCmd)
-	PushCmd.PersistentFlags().String("api-key", "", "The API key to authenticate the organization/system")
+	PushCmd.PersistentFlags().String("api-key", "", "API authentication key for the organization")
 	PushCmd.MarkPersistentFlagRequired("api-key")
-	PushCmd.PersistentFlags().String("base-url", "https://ingestion.pulse.codacy.com", "The API base url")
+	PushCmd.PersistentFlags().String("base-url", "https://ingestion.pulse.codacy.com", "base URL of the API endpoint")
 	PushCmd.PersistentFlags().String("system", "", "repository or component to associate with the event")
 }
 
